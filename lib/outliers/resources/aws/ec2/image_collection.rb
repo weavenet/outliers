f1046e46ae08bb87019df7b66c05dbea6c82855c
Brett Weaver <brett@weav.net>
2013-08-18 12:15:07 -0700

    adding image verification

---
+++ lib/outliers/resources/aws/ec2/image_collection.rb
@@ -0,0 +1,22 @@
+module Outliers
+  module Resources
+    module Aws
+      module Ec2
+        class ImageCollection < Collection
+
+          include Outliers::Filters::Aws::Tags
+
+          def load_all
+            logger.debug "Loading private images owned by this account."
+            connect.images.with_owner(:self).map {|r| resource_class.new r}
+          end
+
+          def self.filters
+            Outliers::Filters::Aws::Tags.filters
+          end 
+
+        end
+      end
+    end
+  end
+end
