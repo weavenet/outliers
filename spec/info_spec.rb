dfe10d8711983a032287a19f4a20433cb25d533a
Brett Weaver <brett@weav.net>
2013-09-15 08:01:20 -0700

    updated pending specs

--- spec/info_spec.rb
+++ spec/info_spec.rb
@@ -6,5 +6,7 @@ describe Outliers::Info do
   end
 
   it "should verify each resource method has a entry in reference.yaml"
-  it "should verify each entry in reference.yaml has a resource"
+  it "should verify each resource entry in reference.yaml has a resource"
+  it "should verify each provider class has a entry in reference.yaml"
+  it "should verify each provider entry in reference.yaml has a valid provider"
 end
