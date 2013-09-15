b62b27864418f3963ad406c0488eccd1af393344
Brett Weaver <brett@weav.net>
2013-09-15 07:39:15 -0700

    updating resource logic

--- spec/info_spec.rb
+++ spec/info_spec.rb
@@ -5,5 +5,6 @@ describe Outliers::Info do
     expect(subject.reference.keys.include?('all')).to be_true
   end
 
-  it "should verify each resource method has a entry in verifications"
+  it "should verify each resource method has a entry in reference.yaml"
+  it "should verify each entry in reference.yaml has a resource"
 end
