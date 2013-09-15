d09499b1a1b0bf2baa478a12a159a2059645554d
Brett Weaver <brett@weav.net>
2013-09-15 08:53:53 -0700

    adding validation on resource file

--- spec/info_spec.rb
+++ spec/info_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe Outliers::Info do
   context "#reference" do
+    before do
+      @resources = []
+      subject.reference.each_pair do |name,data|
+        data['resources'].keys.each do |key|
+          @resources << "#{name}_#{key}"
+        end
+      end
+    end
+
     it "should be able to load reference.yaml" do
       expect(subject.reference.keys.include?('all')).to be_true
     end
@@ -11,13 +20,22 @@ describe Outliers::Info do
     end
 
     it "should verify each resource method has a entry in reference.yaml" do
-      result = []
-      subject.reference.each_pair do |name,data|
-        data['resources'].keys.each do |key|
-          result << "#{name}_#{key}"
+      expect((@resources - ['all_shared']).sort).to eq(Outliers::Resources.list.map {|r| r.to_human}.sort)
+    end
+
+    it "should validate each resource has a verification list and description" do
+      subject.reference.each_value do |provider_data|
+        provider_data['resources'].each_value do |resource_data|
+          expect(resource_data['verifications'].class).to eq(Hash)
+          expect(resource_data['description'].class).to eq(String)
         end
       end
-      expect((result - ['all_shared']).sort).to eq(Outliers::Resources.list.map {|r| r.to_human}.sort)
+    end
+
+    it "should validate each resource has a list of credentials" do
+      subject.reference.each_value do |provider_data|
+        expect(provider_data['credentials'].class).to eq(Hash)
+      end
     end
   end
 end
