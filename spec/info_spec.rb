773bbefbd5440a359605d2d3a8ae6cc16bbdf781
Brett Weaver <brett@weav.net>
2013-09-15 08:35:33 -0700

    validate resources / providers match class vs reference

--- spec/info_spec.rb
+++ spec/info_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 describe Outliers::Info do
-  it "should be able to load reference.yaml" do
-    expect(subject.reference.keys.include?('all')).to be_true
-  end
+  context "#reference" do
+    it "should be able to load reference.yaml" do
+      expect(subject.reference.keys.include?('all')).to be_true
+    end
 
-  it "should verify each resource method has a entry in reference.yaml"
-  it "should verify each resource entry in reference.yaml has a resource"
-  it "should verify each provider class has a entry in reference.yaml"
-  it "should verify each provider entry in reference.yaml has a valid provider"
+    it "should verify each provider class has a entry in reference.yaml" do
+      expect(subject.reference.keys - ['all']).to eq(Outliers::Providers.name_map.keys)
+    end
+
+    it "should verify each resource method has a entry in reference.yaml" do
+      result = []
+      subject.reference.each_pair do |name,data|
+        data['resources'].keys.each do |key|
+          result << "#{name}_#{key}"
+        end
+      end
+      expect((result - ['all_shared']).sort).to eq(Outliers::Resources.list.map {|r| r.to_human}.sort)
+    end
+  end
 end
